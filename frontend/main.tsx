164c541805930b5f55490e65d8fa149d5d215fe3
Steve Canciani <stevecanciani@gmail.com>
2025-05-27 21:56:26 -0400

    firstversion

---
+++ frontend/main.tsx
@@ -0,0 +1,18 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';  // your global styles
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
