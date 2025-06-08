5016100f59ca5254eef7245bbe51ade67ea696a9
Shelle <shelle@Juleens-MacBook-Pro.local>
2025-06-08 17:22:11 -0500

    debugging server ts file for deployment

--- server/dist/server.js
+++ server/dist/server.js
@@ -26,9 +26,9 @@ const startApolloServer = async () => {
         context: authenticateToken
     }));
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.use(express.static(path.join(__dirname, '../../client/dist')));
         app.get('*', (_req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+            res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
         });
     }
     app.listen(PORT, () => {
